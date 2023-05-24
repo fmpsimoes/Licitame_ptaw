f0e1e670cccc5132f410a07c12c5527dd3b589c1
raulsilva2000 <raul.silva@ua.pt>
2023-05-24 17:52:22 +0100

    Teste de Email

--- Licita-me/WebSite/testeemail.php
+++ Licita-me/WebSite/testeemail.php
@@ -1,43 +1,32 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php
-    use PHPMailer\PHPMailer\PHPMailer;
-    use PHPMailer\PHPMailer\Exception;
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-    require './PHPMailer/src/PHPMailer.php';
-    require './PHPMailer/src/Exception.php';
-    require './PHPMailer/src/SMTP.php';
+require './PHPMailer/src/PHPMailer.php';
+require './PHPMailer/src/Exception.php';
+require './PHPMailer/src/SMTP.php';
 
 
-    $mail=new PHPMailer(true);
-    try {
-        $mail->isSMTP();
-        $mail->Host='smtp.gmail.com';
-        $mail->SMTPAuth=true;
-        $mail->Username='noreply.licitame@gmail.com';
-        $mail->Password='exzuoebmwnwzypvm';
-        $mail->SMTPSecure='tls';
-        $mail->Port=587;
+$mail=new PHPMailer(true);
+try {
+    $mail->isSMTP();
+    $mail->Host='smtp.gmail.com';
+    $mail->SMTPAuth=true;
+    $mail->Username='noreply.licitame@gmail.com';
+    $mail->Password='exzuoebmwnwzypvm';
+    $mail->SMTPSecure='tls';
+    $mail->Port=587;
 
-        $mail->setFrom('noreply.licitame@gmail.com','LICITAME');
-        $mail->addAddress('raul.silva@ua.pt','Raul');
+    $mail->setFrom('noreply.licitame@gmail.com','LICITAME');
+    $mail->addAddress('raul.silva@ua.pt','Raul');
 
-        $mail->isHTML(true);
-        $mail->Subject='teste email server licitame';
-        $mail->Body='Corpo de mail de <b>teste</b>';
+    $mail->isHTML(true);
+    $mail->Subject='teste email server licitame';
+    $mail->Body='Corpo de mail de <b>teste</b>';
 
-        $mail->send();
-        echo 'Email sent';
-    } catch (Exception $e) {
-        echo "Email not sent($mail->ErrorInfo)";
-    }
-    ?>
-</body>
-</html>
+    $mail->send();
+    
+} catch (Exception $e) {
+    
+}
+?>
